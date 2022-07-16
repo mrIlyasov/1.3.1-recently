7be6e5a3a7d8204d9c5895d3621e1c7f622343c0
Артур Ильясов <mr.ilyasov90@yandex.ru>
2022-07-16 12:33:00 +0500

    fun hours debug

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,15 +1,16 @@
 fun main() {
 
-    val seconds: Int = 61
- print(agoToText(seconds))
+    val seconds: Int = 75600
+    print(agoToText(seconds))
 
 
 }
 
 fun agoToText(time: Int): String {
     return when (time) {
-        in 0..60 -> "Только что"
-        in 61..60 * 60 -> minutes((time / 60).toInt())
+        in 0..59 -> "Только что"
+        in 60..60 * 60 -> minutes((time / 60).toInt())
+        in 60 * 60..60 * 60 *24 -> hours((time / 60 / 60).toInt())
         else -> "hi"
     }
 
@@ -20,9 +21,8 @@ fun minutes(minutes: Int): String {
     val minutesToString: String = minutes.toString()
     val lastChar: Char = minutesToString[minutesToString.length - 1]
     if (minutesToString.length > 2) {
-         lastByOneChar = minutesToString[minutesToString.length - 2]
-    }
-    else
+        lastByOneChar = minutesToString[minutesToString.length - 2]
+    } else
         lastByOneChar = '0'
     val lastInt: Int = lastChar.digitToInt()
     val lastByOneInt: Int = lastByOneChar.digitToInt()
@@ -39,15 +39,20 @@ fun minutes(minutes: Int): String {
 fun hours(hours: Int): String {
     val hoursToString: String = hours.toString()
     val lastChar: Char = hoursToString[hoursToString.length - 1]
-    val lastByOneChar: Char = hoursToString[hoursToString.length - 2]
+    var lastByOneChar: Char
+    if (hoursToString.length > 2) {
+        lastByOneChar = hoursToString[hoursToString.length - 2]
+    }
+    else
+        lastByOneChar = '0'
     val lastInt: Int = lastChar.digitToInt()
     val lastByOneInt: Int = lastByOneChar.digitToInt()
     if (lastInt == 1 && lastByOneInt != 1) {
-        return " час"
+        return "$hours час"
     } else if (lastInt in 2..4 && lastByOneInt != 1) {
-        return " часа"
+        return "$hours часа"
     } else {
-        return " часов"
+        return "$hours часов"
     }
 
 }
